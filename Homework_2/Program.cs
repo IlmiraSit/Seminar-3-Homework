60bde9b8eca8c5dc9000ce349a65c272d3a71b52
Ilmira Sitdikova <ilmira.sitdikova.1991@mail.ru>
2023-02-11 19:33:30 +0300

    Summa

---
+++ Homework_2/Program.cs
@@ -0,0 +1,11 @@
+﻿//Подсчитать сумму цифр в числе
+Console.WriteLine("Enter number: ");
+int number = int.Parse(Console.ReadLine());
+int sum = 0;
+do
+{
+    sum = sum + number%10;
+    number = number/10;
+}
+while(number>0);
+Console.WriteLine(sum);
