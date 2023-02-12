41366e2a00263266d5cbd2e0a5cfa777dbe8220d
Ilmira Sitdikova <ilmira.sitdikova.1991@mail.ru>
2023-02-12 12:38:05 +0300

    произведения

---
+++ Homework_3/Program.cs
@@ -0,0 +1,12 @@
+﻿//Написать программу вычисления произведения чисел от 1 до N
+Console.WriteLine("Введите число a: ");
+int a =int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число b: ");
+int b =int.Parse(Console.ReadLine());
+int p = 1;
+for (int i = a; i <=b; i++)
+{
+    p = p * i;
+}
+
+Console.WriteLine("Произведения: " +p);
